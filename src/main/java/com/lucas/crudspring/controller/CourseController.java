abb88334042f6b2871a41cf5f1f8594359657608
Lukinh4Z <carvalholnunes@gmail.com>
2022-07-22 15:55:49 -0300

    Updated with course creation

--- src/main/java/com/lucas/crudspring/controller/CourseController.java
+++ src/main/java/com/lucas/crudspring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.lucas.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.crudspring.model.Course;
@@ -29,10 +34,20 @@ public class CourseController {
 
     // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
 
-
+    @PostMapping
+    // poderia usar tanto o ResponseStatus como está.
+    // @ResponseStatus(code = HttpStatus.CREATED)
+    //o @RequestBody vai fazer o mapeamento automatico
+    //das informações recebidas para o model "Course"
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        // return courseRepository.save(course);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
+    }
     
 }
